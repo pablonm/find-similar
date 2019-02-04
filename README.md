b010d44e04c10ae02e0704726db6a82b20814f51
Pablo Miceli <pablomiceli24@gmail.com>
2019-02-03 22:47:09 -0300

    api calls changed to use jsonp

--- README.md
+++ README.md
@@ -17,4 +17,5 @@ This is a small project to test some React libraries:
 You can input a movie/band/show/book/author/game and it shows similar content using [TasteDice API](https://tastedive.com/read/api)
 
 # Live
+
 [find-similar.github.io](find-similar.github.io)
--- src/store/actions/finder.js
+++ src/store/actions/finder.js
@@ -1,18 +1,21 @@
 import * as actionTypes from './actionTypes'
 import axios from 'axios'
+import jsonpAdapter from 'axios-jsonp'
 
-const mockedResponse = {"Similar": {"Info": [{"Name": "Red Hot Chili Peppers", "Type": "music"}], "Results": [{"Name": "John Frusciante", "Type": "music"}, {"Name": "Foo Fighters", "Type": "music"}, {"Name": "Pearl Jam", "Type": "music"}, {"Name": "Nirvana", "Type": "music"}, {"Name": "Guns N' Roses", "Type": "music"}, {"Name": "Sublime", "Type": "music"}, {"Name": "Scar Tissue", "Type": "book"}, {"Name": "Dave Grohl", "Type": "music"}, {"Name": "Audioslave", "Type": "music"}, {"Name": "The Offspring", "Type": "music"}, {"Name": "Flea", "Type": "music"}, {"Name": "Kurt Cobain", "Type": "music"}, {"Name": "Incubus", "Type": "music"}, {"Name": "Rage Against The Machine", "Type": "music"}, {"Name": "System Of A Down", "Type": "music"}, {"Name": "AC/DC", "Type": "music"}, {"Name": "Oasis", "Type": "music"}, {"Name": "Aerosmith", "Type": "music"}, {"Name": "Slash", "Type": "music"}, {"Name": "Lenny Kravitz", "Type": "music"}]}}
+const api = axios.create({
+    adapter: jsonpAdapter,
+    callbackParamName: 'callback',
+});
 
 export const findSimilar = (to) => {
     return (dispatch) => {
         dispatch(findSimilarStart())
-        axios.get(process.env.REACT_APP_TASTEDIVE_API_URL + '?q='+ encodeURI(to) +'&k=' + process.env.REACT_APP_TASTEDIVE_API_KEY)
+        api.get(process.env.REACT_APP_TASTEDIVE_API_URL + '?q='+ encodeURI(to) +'&k=' + process.env.REACT_APP_TASTEDIVE_API_KEY)
             .then(response => {
                 dispatch(findSimilarSuccess(response));
             })
             .catch(error => {
-                //dispatch(findSimilarFail(error));
-                dispatch(findSimilarSuccess(mockedResponse))
+                dispatch(findSimilarFail(error));
             })
     }
 }
@@ -33,6 +36,6 @@ export const findSimilarFail = (error) => {
 export const findSimilarSuccess = (response) => {
     return {
         type: actionTypes.FIND_SIMILAR_SUCCESS,
-        payload: { results: response.Similar.Results }
+        payload: { results: response.data.Similar.Results }
     }
 }
